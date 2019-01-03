c8b9ad3ca9901ff2c1de61afd5db028459f54738
Ondabega <thomas.abega-romieu@student.ecp.fr>
2019-01-04 00:18:08 +0100

    identification

---
+++ moncompte/identification.php
@@ -0,0 +1,66 @@
+<?php
+  ob_start();
+  include_once "../needed.php";
+  $errorMessage = '';
+
+  if(!empty($_POST))
+  {
+    if(!empty($_POST['login']) && !empty($_POST['password']))
+    {
+      $query = $bdd -> prepare('SELECT * FROM utilisateur WHERE identifiant = ?');
+      $query -> execute(array($_POST['login']));
+      if($Data = $query -> fetch()){
+        if(crypt(strtolower($_POST['password']),"immobilier") == $Data['password']){
+          session_start();
+          $_SESSION['login'] = true;
+          $_SESSION['id'] = $Data['id'];
+          $_SESSION['nom'] = $Data['nom'];
+          $_SESSION['prenom'] = $Data['prenom'];
+
+          ob_end_clean();
+
+          if(isset($_GET['redirection'])){
+            header('Location: '.$url."/".$_GET['redirection']);
+          }else{
+            header('Location: '.$url."/moncompte");
+          }
+          exit();
+        }else{
+          $errorMessage = 'Mauvais password';
+                  }
+      }else{
+        $errorMessage = 'Mauvais login';
+      }
+    }
+      else
+    {
+      $errorMessage = 'Veuillez inscrire vos identifiants';
+    }
+  }
+
+	drawHeader('moncompte');
+  if(isset($_GET['redirection'])){
+    $direction="/moncompte/identification.php?redirection=".$_GET['redirection'];
+  }else{
+    $direction="/moncompte/identification.php";
+  }
+?>
+<h3>Identifiez-vous</h3>
+    <form action=<?php echo $direction?> method="post" style="padding-top:20px; width : 400px;" >
+        <?php
+          if(!empty($errorMessage))
+          {
+            echo '<p>', htmlspecialchars($errorMessage) ,'</p>';
+          }
+        ?>
+       <div clas="form-group">
+          <label for="login">Login :</label>
+          <input type="text" name="login" id="login" value="" class="form-control" />
+        </div>
+        <br>
+        <div class="form-group">
+          <label for="password">Password :</label>
+          <input type="password" name="password" id="password" value="" class="form-control" />
+        </div>
+        <input type="submit" name="submit" value="Connexion" class="btn btn-default"/>
+    </form>
--- needed.php
+++ needed.php
@@ -1,3 +1,5 @@
 <?php
+session_start();
 $bdd = new PDO('mysql:host=localhost;dbname=immobilier;charset=utf8', 'tavg', 'lacolloc');
+$url ="http://immobilier.fr/" ; 
 ?>
