2b90509c2514c3957acf9cf6a4a7db668219b8c6
anonym0017 <ondiguidominique14@gmail.com>
2019-01-04 04:45:46 +0100

    contact

---
+++ contact.php
@@ -0,0 +1,40 @@
+<meta charset="utf-8">
+<?php
+	extract($_POST);
+	$errors = [];
+
+	if (!array_key_exists('nom', $_POST) || $_POST['nom'] == '') {
+		# code...
+		$errors['nom'] = "Vous n'avez pas renseigné votre nom";
+	}
+
+	if (!array_key_exists('email', $_POST) || $_POST['email'] == '' || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		# code...
+		$errors['email'] = "Vous n'avez pas renseigné une adresse mail valide";
+	}
+
+	if (!array_key_exists('message', $_POST) || $_POST['message'] == '') {
+		# code...
+		$errors['message'] = "Vous n'avez pas saisi de message";
+	}
+
+	session_start();
+
+	if (!empty($errors)) {
+		# code...
+		$_SESSION['errors'] = $errors;
+		$_SESSION['inputs'] = $_POST;
+		header('Location: index.php#contact');
+	}
+	else{
+		$_SESSION['success'] = 1;
+		$to = 'ondiguidominique14@gmail.com';
+		$subject = 'Contact du site';
+		$msg =
+			"Salut, Domi! Un visiteur du nom de $civilite $nom, dont l'adresse mail est $email a pris contact de ton site et son message est le suivant: \n
+			$message";
+		mail($to, $subject, $msg);
+		header('Location: index.php#contact');
+	}
+
+?>
