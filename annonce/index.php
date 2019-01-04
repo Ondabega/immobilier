bfc6c9b32848e2e22a52efd96318d29b46ec7f9d
Ondabega <thomas.abega-romieu@student.ecp.fr>
2019-01-04 05:58:16 +0100

    nn

--- annonce/index.php
+++ annonce/index.php
@@ -12,13 +12,11 @@ include_once "../needed.php";
 <body>
 
 	<!-- navigation -->
-	<div class="header" style="background: url(../images/cover2.jpg) no-repeat center center; background-size: cover; height: 500px">
+	<div class="header" style="background: url(../images/cover2.jpg) no-repeat center center; background-size: cover; height: 600px">
 		<nav class="nav" id="nav">
 			<div class="element">
 			<a href= <?php echo $url ?> class="selt" onclick="myFunction()">Accueil</a>
 				<a href="#contact" class="selt" onclick="myFunction()">Contact</a>
-        <a href="#portefolio" class="selt" onclick="myFunction()">Achat</a>
-				<a href="#contact" class="selt" onclick="myFunction()">Location</a>
         	<?php if(!empty($_SESSION['login'])){ echo "<a href='../moncompte/deconnexion.php' class='selt' id='connect' onclick='myFunction()'>Deconnexion</a>" ;}
 				else{ echo "<a href='../moncompte/identification.php' class='selt' id='connect' onclick='myFunction()'>Connexion</a>"; }?>
         <?php if(!empty($_SESSION['admin'])){ echo "<a href='ajout.php' class='selt' id='connect' onclick='myFunction()'>Espace admin</a>" ;} ?>
--- moncompte/identification.php
+++ moncompte/identification.php
@@ -46,29 +46,68 @@
     $direction="/moncompte/identification.php";
   }
 ?>
-<div class="row">
-    <div class="col-md-2"></div>
-  <div class="col-md-4">
-<h3>Identifiez-vous</h3>
 
-    <form action=<?php echo $direction?> method="post" style="padding-top:20px; width : 400px;" >
-        <?php
-          if(!empty($errorMessage))
-          {
-            echo '<p>', htmlspecialchars($errorMessage) ,'</p>';
-          }
-        ?>
-       <div clas="form-group">
-          <label for="login">Login :</label>
-          <input type="text" name="login" id="login" value="" class="form-control" />
-        </div>
-        <br>
-        <div class="form-group">
-          <label for="password">Password :</label>
-          <input type="password" name="password" id="password" value="" class="form-control" />
-        </div>
-        <input type="submit" name="submit" value="Connexion" class="btn btn-default"/>
-    </form>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../Style/style1.css">
+	<title>Untitled</title>
+</head>
+
+<div class="header" style="background: url(../images/cover4.jpg) no-repeat center center; background-size: cover;width:100%; height: 600px">
+  <nav class="nav" id="nav">
+    <div class="element">
+      <a href= <?php echo $url ?> class="selt" onclick="myFunction()">Accueil</a>
+      <a href="annonce\index.php" class="selt" onclick="myFunction()">Annonces</a>
+      <a href="contact-form.php" class="selt" onclick="myFunction()">Contact</a>
+      <?php if(!empty($_SESSION['login'])){ echo "<a href='/moncompte/deconnexion.php' class='selt' id='connect' onclick='myFunction()'>Deconnexion</a>"; }
+    else{ echo "<a href='/moncompte/identification.php' class='selt' id='connect' onclick='myFunction()'>Connexion</a>" ; } ?>
+    </div>
+    <div class="logow">
+      <a href="#index" class="logo" > Audrey Brezout </a>
+
+    </div>
+  </nav>
+  <div style="margin-top: 10%; ">
+    <div class="row">
+        <div class="col-md-4"></div>
+      <div class="col-md-4">
+    <h3>Identifiez-vous</h3>
+
+        <form action=<?php echo $direction?> method="post" style="padding-top:20px; width : 400px;" >
+            <?php
+              if(!empty($errorMessage))
+              {
+                echo '<p>', htmlspecialchars($errorMessage) ,'</p>';
+              }
+            ?>
+           <div clas="form-group">
+              <label for="login">Login :</label>
+              <input type="text" name="login" id="login" value="" class="form-control" />
+            </div>
+            <br>
+            <div class="form-group">
+              <label for="password">Password :</label>
+              <input type="password" name="password" id="password" value="" class="form-control" />
+            </div>
+            <input type="submit" name="submit" value="Connexion" class="btn btn-default"/>
+        </form>
+      </div>
+      <div class="col-md-4"></div>
+    </div>
+
   </div>
-  <div class="col-md-6"></div>
 </div>
+
+<!-- add javascript for navbar onscroll -->
+<script type="text/javascript">
+  window.onscroll = function() {myFunction()};
+
+  function myFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.getElementById("nav").className = "navbar";
+    }
+    else {
+      document.getElementById("nav").className = "";
+    }
+  }
+</script>
