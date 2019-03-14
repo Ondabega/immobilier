ce70628e1f181116425ab5e530bb33bfb972103b
Ondabega <thomas.abega-romieu@student.ecp.fr>
2019-03-14 21:02:41 +0100

    deconexion pb

--- moncompte/deconnexion.php
+++ moncompte/deconnexion.php
@@ -1,7 +1,10 @@
 <?php
 include_once "../needed.php";
 $_SESSION['login']=false;
-session_start();
+if(!isset($_SESSION))
+    {
+        session_start();
+    } 
 session_destroy();
 unset($_SESSION);
 header("Location: ".$url."");
