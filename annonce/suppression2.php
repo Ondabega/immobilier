bfc38b8aa65053414dd4978ecdb07b7159b9aac1
Ondabega <thomas.abega-romieu@student.ecp.fr>
2019-04-04 21:54:09 +0200

    gg

--- annonce/suppression2.php
+++ annonce/suppression2.php
@@ -40,7 +40,7 @@ $nb=0; ?>
 <?php
 if(empty($_SESSION['login']))
 { ?>
-  <h2>Villes</h2>
+  <h2>Biens</h2>
   <h4>Vous devez être connecté pour accéder à cette partie.</h4>
   <a href="/moncompte/identification.php?redirection=annonce/ajout.php"><button class="btn btn-default">Se connecter</button></a>
   <a href="/index.php" class="btn btn-default"> Accueil</a>
@@ -49,28 +49,167 @@ if(empty($_SESSION['login']))
 else
 {
   if(isset($_POST['supprimer'])){
+    $quer= $bdd -> prepare('SELECT * FROM vente WHERE id=?');
+    $quer -> execute(array($_POST['id']));
+    $Data= $quer -> fetch();
+    if($Data['image'] != 0){
+      remove_file($bdd,$Data['image']);
+    }
+    if($Data['image2'] != 0){
+      remove_file($bdd,$Data['image2']);
+    }
+    if($Data['image3'] != 0){
+      remove_file($bdd,$Data['image3']);
+    }
+    if($Data['image4'] != 0){
+      remove_file($bdd,$Data['image4']);
+    }
+    if($Data['image5'] != 0){
+      remove_file($bdd,$Data['image5']);
+    }
+    if($Data['image6'] != 0){
+      remove_file($bdd,$Data['image6']);
+    }
+    if($Data['image7'] != 0){
+      remove_file($bdd,$Data['image7']);
+    }
+    if($Data['image8'] != 0){
+      remove_file($bdd,$Data['image8']);
+    }
 
-    $query = $bdd -> prepare('DELETE FROM ville WHERE id=?');
+    $query = $bdd -> prepare('DELETE FROM vente WHERE id=?');
     $query -> execute(array($_POST['id']));
-    success('Supprimé','La ville a bien été supprimée.');
 
-  }elseif(isset($_POST['modifier'])){
+    success('Supprimé','Le bien a bien été supprimée.');
 
-  $quert= $bdd -> prepare('SELECT * FROM villes WHERE id=?');
+}elseif(isset($_POST['img-reset'])) {
+  $query= $bdd -> prepare('SELECT * FROM vente WHERE id=?');
+  $query -> execute(array($_POST['id']));
+  $Data= $query -> fetch();
+  remove_file($bdd,$Data['image']);
+  $query = $bdd -> prepare('UPDATE vente SET image = NULL WHERE id = ?');
+  $query -> execute(array($_POST['id']));
+  success('Supprimé','L\'image a été supprimée.');
+}elseif(isset($_POST['img-reset2'])) {
+  $query= $bdd -> prepare('SELECT * FROM vente WHERE id=?');
+  $query -> execute(array($_POST['id']));
+  $Data= $query -> fetch();
+  remove_file($bdd,$Data['image2']);
+  $query = $bdd -> prepare('UPDATE vente SET image = NULL WHERE id = ?');
+  $query -> execute(array($_POST['id']));
+  success('Supprimé','L\'image a été supprimée.');
+}elseif(isset($_POST['img-reset3'])) {
+  $query= $bdd -> prepare('SELECT * FROM vente WHERE id=?');
+  $query -> execute(array($_POST['id']));
+  $Data= $query -> fetch();
+  remove_file($bdd,$Data['image4']);
+  $query = $bdd -> prepare('UPDATE vente SET image = NULL WHERE id = ?');
+  $query -> execute(array($_POST['id']));
+  success('Supprimé','L\'image a été supprimée.');
+}elseif(isset($_POST['img-reset5'])) {
+  $query= $bdd -> prepare('SELECT * FROM vente WHERE id=?');
+  $query -> execute(array($_POST['id']));
+  $Data= $query -> fetch();
+  remove_file($bdd,$Data['image2']);
+  $query = $bdd -> prepare('UPDATE vente SET image = NULL WHERE id = ?');
+  $query -> execute(array($_POST['id']));
+  success('Supprimé','L\'image a été supprimée.');
+}elseif(isset($_POST['img-reset6'])) {
+  $query= $bdd -> prepare('SELECT * FROM vente WHERE id=?');
+  $query -> execute(array($_POST['id']));
+  $Data= $query -> fetch();
+  remove_file($bdd,$Data['image2']);
+  $query = $bdd -> prepare('UPDATE vente SET image = NULL WHERE id = ?');
+  $query -> execute(array($_POST['id']));
+  success('Supprimé','L\'image a été supprimée.');
+}elseif(isset($_POST['img-reset7'])) {
+  $query= $bdd -> prepare('SELECT * FROM vente WHERE id=?');
+  $query -> execute(array($_POST['id']));
+  $Data= $query -> fetch();
+  remove_file($bdd,$Data['image2']);
+  $query = $bdd -> prepare('UPDATE vente SET image = NULL WHERE id = ?');
+  $query -> execute(array($_POST['id']));
+  success('Supprimé','L\'image a été supprimée.');
+}elseif(isset($_POST['img-reset8'])) {
+  $query= $bdd -> prepare('SELECT * FROM vente WHERE id=?');
+  $query -> execute(array($_POST['id']));
+  $Data= $query -> fetch();
+  remove_file($bdd,$Data['image2']);
+  $query = $bdd -> prepare('UPDATE vente SET image = NULL WHERE id = ?');
+  $query -> execute(array($_POST['id']));
+  success('Supprimé','L\'image a été supprimée.');
+}elseif(isset($_POST['modifier'])){
+
+  $quert= $bdd -> prepare('SELECT * FROM vente WHERE id=?');
   $quert -> execute(array($_POST['id']));
   $Data= $quert -> fetch();
+  $file=$Data['image'];
+  $file2=$Data['image2'];
+  $file3=$Data['image3'];
+  $file4=$Data['image4'];
+  $file5=$Data['image5'];
+  $file6=$Data['image7'];
+  $file8=$Data['image8'];
+  if($_FILES['fichier']['name'] != ""){
+    if($Data['image'] != NULL ){remove_file($bdd,$Data['image']); }
+    $file=upload($bdd,'fichier',"../../ressources","idees",5048576,array( 'jpg' , 'jpeg' , 'gif' , 'png' , 'JPG' , 'JPEG' , 'GIF' , 'PNG' ));
+    if($file <0 ){ $file= $Data['image'];}
+  }
+  if($_FILES['fichier2']['name'] != ""){
+    if($Data['image2'] != NULL ){remove_file($bdd,$Data['image2']); }
+    $file2=upload($bdd,'fichier2',"../../ressources","idees",5048576,array( 'jpg' , 'jpeg' , 'gif' , 'png' , 'JPG' , 'JPEG' , 'GIF' , 'PNG' ));
 
-  $query = $bdd -> prepare("UPDATE ville SET nom=:nom WHERE id = :id ");
-  $query->bindValue('nom', $_POST['nom'],PDO::PARAM_STR);
+  }
+  if($_FILES['fichier3']['name'] != ""){
+    if($Data['image3'] != NULL ){remove_file($bdd,$Data['image3']); }
+    $file3=upload($bdd,'fichier3',"../../ressources","idees",5048576,array( 'jpg' , 'jpeg' , 'gif' , 'png' , 'JPG' , 'JPEG' , 'GIF' , 'PNG' ));
+
+  }
+  if($_FILES['fichier4']['name'] != ""){
+    if($Data['image4'] != NULL ){remove_file($bdd,$Data['image4']); }
+    $file4=upload($bdd,'fichier4',"../../ressources","idees",5048576,array( 'jpg' , 'jpeg' , 'gif' , 'png' , 'JPG' , 'JPEG' , 'GIF' , 'PNG' ));
+  }
+  if($_FILES['fichier5']['name'] != ""){
+    if($Data['image5'] != NULL ){remove_file($bdd,$Data['image5']); }
+    $file5=upload($bdd,'fichier5',"../../ressources","idees",5048576,array( 'jpg' , 'jpeg' , 'gif' , 'png' , 'JPG' , 'JPEG' , 'GIF' , 'PNG' ));
+
+  }
+  if($_FILES['fichier6']['name'] != ""){
+    if($Data['image6'] != NULL ){remove_file($bdd,$Data['image6']); }
+    $file2=upload($bdd,'fichier6',"../../ressources","idees",5048576,array( 'jpg' , 'jpeg' , 'gif' , 'png' , 'JPG' , 'JPEG' , 'GIF' , 'PNG' ));
+
+  }
+  if($_FILES['fichier7']['name'] != ""){
+    if($Data['image7'] != NULL ){remove_file($bdd,$Data['image7']); }
+    $file7=upload($bdd,'fichier7',"../../ressources","idees",5048576,array( 'jpg' , 'jpeg' , 'gif' , 'png' , 'JPG' , 'JPEG' , 'GIF' , 'PNG' ));
+
+  }
+  if($_FILES['fichier8']['name'] != ""){
+    if($Data['image8'] != NULL ){remove_file($bdd,$Data['image8']); }
+    $file8=upload($bdd,'fichier8',"../../ressources","idees",5048576,array( 'jpg' , 'jpeg' , 'gif' , 'png' , 'JPG' , 'JPEG' , 'GIF' , 'PNG' ));
+
+  }
+
+  $query = $bdd -> prepare("UPDATE vente SET type=:type ,transversalisation = :transversalisation,retenue= :retenue,respo_rea=:respo_rea, situation_actuelle= :situation_actuelle, situation_proposee= :situation_proposee ,image = :image , nbidees= :nbidees, image2= :image2 WHERE id = :id ");
+  $query->bindValue('type', $_POST['type'],PDO::PARAM_STR);
+  $query->bindValue('transversalisation', $_POST['transversalisation'],PDO::PARAM_INT);
+ $query->bindValue('retenue', $_POST['retenue'],PDO::PARAM_INT);
+ $query->bindValue('respo_rea', $_POST['respo_rea'],PDO::PARAM_INT);
+ $query->bindValue('situation_actuelle', $_POST['situation_actuelle'],PDO::PARAM_STR);
+ $query->bindValue('situation_proposee', $_POST['situation_proposee'],PDO::PARAM_STR);
  $query->bindValue('id', $_POST['id'],PDO::PARAM_INT);
+ $query->bindValue('image',$file,PDO::PARAM_INT);
+ $query->bindValue('nbidees',$_POST['nbidees'],PDO::PARAM_INT);
+ $query->bindValue('image2',$file2,PDO::PARAM_INT);
  $query->execute();
 
   if($query ==false){
     warning('Erreur','Les données entrées ne sont pas conformes.');
   }else{
-    success('Modifié','La ville a bien été mise à jour.');
+    success('Modifié','La question a bien été mise à jour.');
   }
-}
+
+  }
 
   ?>
 
