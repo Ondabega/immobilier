43cfb8eacd9a30be57f97fce2eb551f9ba6bd4e1
Ondabega <thomas.abega-romieu@student.ecp.fr>
2019-05-04 09:49:37 +0200

    n

--- index.php
+++ index.php
@@ -81,7 +81,7 @@ include_once "needed.php";
 			else{ echo "<a href='../moncompte/identification.php' class='selt' onclick='myFunction()'>Connexion</a>"; }?>
 			</div>
 		</nav>
-	
+
 	</div>
 
 	<!-- add javascript for navbar onscroll -->
@@ -101,7 +101,7 @@ include_once "needed.php";
 	<!-- pied de page -->
 <footer class="footer" style="margin-bottom: 0px;">
 			<div class="row">
-            			<h4>boutikimmo@gmail.com</h4>
+            			<h4>boutikimo@gmail.com</h4>
           	</div>
 
 				<hr class="separator" style="margin-left: 47%;">
